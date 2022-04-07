5ae1519061237d1a29b61d6745a1b6f4d170a9d2
Solórzano <lorenzo.solorzano@accenture.com>
2022-04-07 16:58:26 -0600

    Footer Hardcoded

---
+++ ui.apps/target/generated-sources/htl/org/apache/sling/scripting/sightly/apps/mysite/components/footer/footer__002e__html.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ ******************************************************************************/
+package org.apache.sling.scripting.sightly.apps.mysite.components.footer;
+
+import java.io.PrintWriter;
+import java.util.Collection;
+import javax.script.Bindings;
+
+import org.apache.sling.scripting.sightly.render.RenderUnit;
+import org.apache.sling.scripting.sightly.render.RenderContext;
+
+public final class footer__002e__html extends RenderUnit {
+
+    @Override
+    protected final void render(PrintWriter out,
+                                Bindings bindings,
+                                Bindings arguments,
+                                RenderContext renderContext) {
+// Main Template Body -----------------------------------------------------------------------------
+
+out.write("<footer class=\"cmp-footer\">\r\n    <div class=\"container\">\r\n        <div class=\"cmp-footer__main-nav\">\r\n            <div class=\"row\">\r\n                <div class=\"col-lg-7\">\r\n                    <nav class=\"nav\">\r\n                        <a class=\"nav-link p-0\" href=\"#\">About Jolt</a>\r\n                        <a class=\"nav-link p-0\" href=\"#\">Contact us</a>\r\n                        <a class=\"nav-link p-0\" href=\"#\">Careers</a>\r\n                        <a class=\"nav-link p-0\" href=\"#\">Meet our partners</a>\r\n                    </nav>\r\n                </div>\r\n                <div class=\"col-lg-5\">\r\n                    <ul class=\"list-inline d-lg-flex justify-content-end\">\r\n                        <li class=\"list-inline-item\"><a href=\"#\"><span class=\"d-block cmp-footer__icon cmp-footer__icon-fb\"></span></a></li>\r\n                        <li class=\"list-inline-item\"><a href=\"#\"><span class=\"d-block cmp-footer__icon cmp-footer__icon-tw\"></span></a></li>\r\n                        <li class=\"list-inline-item\"><a href=\"#\"><span class=\"d-block cmp-footer__icon cmp-footer__icon-lk\"></span></a></li>\r\n                        <li class=\"list-inline-item\"><a href=\"#\"><span class=\"d-block cmp-footer__icon cmp-footer__icon-yt\"></span></a></li>\r\n                        <li class=\"list-inline-item\"><a href=\"#\"><span class=\"d-block cmp-footer__icon cmp-footer__icon-in\"></span></a></li>\r\n                    </ul>\r\n                </div>\r\n            </div>\r\n        </div>\r\n        <div class=\"cmp-footer__secondary-nav\">\r\n            <nav class=\"cmp-navigation__nav nav\">\r\n                <a class=\"nav-link p-0\" href=\"#\">Feedback</a>\r\n                <a class=\"nav-link p-0\" href=\"#\">Help</a>\r\n                <a class=\"nav-link p-0\" href=\"#\">Privacy statement</a>\r\n                <a class=\"nav-link p-0\" href=\"#\">Cookies</a>\r\n                <a class=\"nav-link p-0\" href=\"#\">Trademarks</a>\r\n                <a class=\"nav-link p-0\" href=\"#\">Supply chain transparency</a>\r\n                <a class=\"nav-link p-0\" href=\"#\">Sitemap</a>\r\n            </nav>\r\n        </div>\r\n        <div class=\"cmp-footer__copyright\">\u00A92022 Jolt Systems, Inc.</div>\r\n    </div>\r\n</footer>");
+
+
+// End Of Main Template Body ----------------------------------------------------------------------
+    }
+
+
+
+    {
+//Sub-Templates Initialization --------------------------------------------------------------------
+
+
+
+//End of Sub-Templates Initialization -------------------------------------------------------------
+    }
+
+}
+
